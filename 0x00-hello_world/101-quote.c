2f3ecc94c2e119cf2878b609785d37bd6f7baede
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-15 16:28:31 -0700

    my hello work

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Piece of art is useful
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
