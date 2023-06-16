746c0c28f53613df3ad182fc5ad06f7159d5eb77
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-16 06:17:00 -0700

    my avariables work

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 m = n % 10;
 if (m > 5)
-printf("last digit %d is %d and is greater than 5\n", n, m);
-else if (m == 0)
-printf("last digit %d is %d is 0\n", n, m);
-else if (m < 6 && m != 0)
-printf("last digit %d is %d and is less than 6 and not 0\n", n, m);
+printf("Last digit %d is %d and is greater than 5\n", n, m);
+if (m == 0)
+printf("Last digit %d is %d is 0\n", n, m);
+if (m < 6 && m != 0)
+printf("Last digit %d is %d and is less than 6 and not 0\n", n, m);
 return (0);
 }
