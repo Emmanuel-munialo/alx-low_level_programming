12a6562bd8e817cabb85ed8cc5204feb6712a535
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-16 12:32:35 -0700

    my variables work

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Prints all single digit numbers of base 10
+* Return: 0
+*/
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
