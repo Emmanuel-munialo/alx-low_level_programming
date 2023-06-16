a7d48ba927fa0a5dc459cf859289689326b67af5
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-16 09:00:40 -0700

    my variableswork

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+if (low != e && low != q)
+putchar(low);
+putchar('\n');
+return (0);
