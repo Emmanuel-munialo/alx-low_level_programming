79da095fbe56001c05d704e1af4a43fd4627b641
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-18 06:56:21 -0700

    my hello work

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+int d;
+for (d = 0; d <= 9; i++)
+putchar(d);
+if (d != '9')
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
+}
