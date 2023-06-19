7f0f0a4d8d196561398459c6524dcfb736c69d2b
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-19 16:57:56 -0700

    my variables work

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ for (j = i; j <= 98; j++)
 {
 for (j = i; j <= 99; j++)
 putchar((i % 10) + '0');
-putchar((i / 10) + '0')
+putchar((i / 10) + '0');
 putchar(' ');
-putchar(((j / 10) + '0');
+putchar((j / 10) + '0');
 putchar((j % 10) + '0');
 if (i == 99 && j == 99)
 continue;
