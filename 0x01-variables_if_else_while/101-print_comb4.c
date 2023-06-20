7ed13b0102d6aa9cb47af9abbbdcb3ed745fe60d
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-19 17:10:48 -0700

    my variables work

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,14 +8,10 @@
 int main(void)
 {
 int i, j, m;
-for (i = '0'; i <= '7'; i++)
-{
-for (j = i + 1; j <= '8'; j++)
-{
-for (m = j + 1; m <= '9'; m++)
-{
-if ((j != i) != m)
-{
+for (i = '0'; i <= '7'; i++) {
+for (j = i + 1; j <= '8'; j++) {
+for (m = j + 1; m <= '9'; m++) {
+if ((j != i) != m) {
 putchar(i);
 putchar(j);
 putchar(m);
