41c03ed0fb00b3534981adb78a24aee775f9a736
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-21 10:01:45 -0700

    C programming

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+* prints 10 times the alphabet, in lowercase, followed by a new line.
+*/
+void print_alphabet_x10(void)
+{
+int line = l, char = ch;
+for (l = 0; l <= 9; ++l)
+{
+for (ch = 'a'; ch <= 'z'; ++ch)
+_putchar(ch);
+_putchar('\n');
+}
+}
