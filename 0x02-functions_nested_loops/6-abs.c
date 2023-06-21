a331b673a6d2da366e9ce5badfd37e65630d1ce1
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-21 11:42:38 -0700

    C programming

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - a function that prints the last digit of a number.
+ * Return: 0
+ */
+int main(void)
+{
+int b;
+b _abs(-1);
+printf("%d\n", b);
+b _abs(0);
+printf("%d\n", b);
+b _abs(1);
+printf("%d\n", b);
+b _abs(-98);
+printf("%d\n", b);
+return (0);
+}
