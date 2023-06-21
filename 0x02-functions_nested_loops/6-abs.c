d900b51f63a306005e943c31f5d6a5e9e5f28057
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-21 13:05:23 -0700

    C programming

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,12 @@
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
 /**
-* main -  a function that computes the absolute value of an integer.
-* Prototype: int _abs(int);
-*
-* Return: Always 0.
+ * _abs - function that computes the absolute value of an integer
+* @n: takes in integer type input for function
+* Return: Always 0 (Success)
 */
-int main(void)
+int _abs(int n)
 {
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+if (n < 0)
+n = (-1) * n;
+return (n);
 }
