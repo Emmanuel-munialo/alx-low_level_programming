94a65eab7cd8c229b0517044dfc09ac0fa6c984f
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-06-24 11:39:49 -0700

    c programming

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - The standard library provides a similar function
+* Return: 1 for upper letter or 0 for any else
+*/
+int _isupper(void)
+{
+int x;
+{
+if (x >= 65 && x <=)
+{
+return (0);
+}
+return (0);
+}
