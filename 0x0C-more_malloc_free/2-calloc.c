e3a78087a49cb849acceb33957f8e06082d91787
Emmanuel-munialo <emmanuelmunialo256@gmail.com>
2023-07-14 15:44:56 -0700

    coding

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -29,9 +29,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 char *ptr;
 if (nmemb == 0 || size == 0)
 return (NULL);
-ptr = malloc(size * nmemb)
+ptr = malloc(size * nmemb);
 if (ptr == NULL)
 return (NULL);
 _memset(ptr, 0, nmemb * size);
 return (ptr);
+}
 }
